0a90901c914ce000ab6433b264cf7832d0a98b2f
DyKiVy <kirillsihka@gmail.com>
2022-12-07 19:11:07 +0500

    Метод расчёски

--- Метод расчёски.py
+++ Метод расчёски.py
@@ -1,12 +1,19 @@
-def CombSort(data):
-    gap = len(data)
+def CombSort(A):
+    step = len(A)
     swaps = True
-    while gap > 1 or swaps:
-        gap = max(1, int(gap / 1.25))  # minimum gap is 1
+    while step > 1 or swaps:
+        gap = max(1, int(step / 1.25))  # minimum gap is 1
         swaps = False
-            for i in range(len(data) - gap):
-                    j = i + gap
-                    if data[i] > data[j]:
-                        data[i], data[j] = data[j], data[i]
-                        swaps = True
-    return data
+        for i in range(len(A) - step):
+            j = i + step
+            if A[i] > A[j]:
+                A[i], A[j] = A[j], A[i]
+                swaps = True
+    return A
+
+
+print("Введите элементы массива")
+a = []
+a = input().split()
+print("Отсортированный массив")
+print(CombSort(a))
